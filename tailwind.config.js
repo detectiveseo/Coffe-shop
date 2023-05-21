7fa58a21bd6cc33dc2b88f5390179820663e788d
detectiveseo <detectiveseo1@gmail.com>
2023-05-21 10:48:26 +0600

    connecting with github

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      colors: {
+        "light-orange": "#db9457",
+      }
+    }
+  },
+  plugins: [],
+}
+
